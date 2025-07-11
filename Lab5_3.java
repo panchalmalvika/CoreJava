83c4dceb7ec563a528f63d838a5bf50753296365
Panchal Malvika <panchalmalvika944@gmail.com>
2025-07-11 10:03:59 +0530

    11-07-2025

---
+++ Lab5_3.java
@@ -0,0 +1,22 @@
+package assignment;
+
+public class Lab5_3 {
+		static int a=10;
+		static int b=20;
+		void geta()
+		{
+			System.out.println("A : "+a);
+			System.out.println("B : "+b);
+		}
+		static
+		{
+			System.out.println("Addition : "+(a+b));
+		}
+		static
+		{
+			System.out.println("Multiplication : "+(a*b));
+		}
+	public static void main(String[] args) {
+		Lab5_3 l1Lab5_3=new Lab5_3();
+	}
+}

4d39bd0ad9896bf7f342b9c4795364715f78bbf5
Panchal Malvika <panchalmalvika944@gmail.com>
2025-07-14 11:08:32 +0530

    14-07-2025

---
+++ ExceptionDemo.java
@@ -0,0 +1,30 @@
+package com.exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionDemo {
+	public static void main(String[] args) {
+		System.out.println("Start code");
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+	try {
+		System.out.print("Enter A : ");
+		a=sc.nextInt();
+		System.out.print("Enter B : ");
+		b=sc.nextInt();
+		c=a/b;
+		System.out.println("Division : "+c);
+	}
+	catch(ArithmeticException e)
+	{
+		System.out.println("Exception caught : "+e);
+	}
+	catch(InputMismatchException e)
+	{
+		System.out.println("Exception caught : "+e);
+	}
+	System.out.println("End code");
+	}
+	
+}

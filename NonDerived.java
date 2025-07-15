deec8915020e58ac07c4fd8c7e452f4a96076e07
Panchal Malvika <panchalmalvika944@gmail.com>
2025-07-15 10:02:49 +0530

    15-07-2025

---
+++ NonDerived.java
@@ -0,0 +1,14 @@
+package com.p1;
+
+public class NonDerived {
+
+	public NonDerived()
+	{
+		Protection p=new Protection();
+		System.out.println("NonDerived's constructor called");
+		System.out.println("n : "+p.n);
+		System.out.println("n_pro : "+p.n_pro);
+		System.out.println("n_pub : "+p.n_pub);
+		//System.out.println("n_pri : "+p.n_pri);
+	}
+}

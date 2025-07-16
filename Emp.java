821cbb21c155cc6fd3270ba08e00d73e312481bf
Panchal Malvika <panchalmalvika944@gmail.com>
2025-07-16 11:50:31 +0530

    16-07-2025

---
+++ Emp.java
@@ -0,0 +1,26 @@
+package com.collection;
+
+public class Emp {
+	private String Ename;
+	private int Eid;
+	private double Salary;
+	public String getEname() {
+		return Ename;
+	}
+	public void setEname(String Ename) {
+		this.Ename = Ename;
+	}
+	public int getEid() {
+		return Eid;
+	}
+	public void setEid(int Eid) {
+		this.Eid = Eid;
+	}
+	public double getSalary() {
+		return Salary;
+	}
+	public void setSalary(double Salary) {
+		this.Salary = Salary;
+	}
+	
+}

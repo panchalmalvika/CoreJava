f4c491107bfe045589425f4c237fa98b4fc49c31
Panchal Malvika <panchalmalvika944@gmail.com>
2025-07-17 11:05:06 +0530

    17-07-2025

---
+++ Arithmatic.java
@@ -0,0 +1,17 @@
+package Test.first;
+
+public class Arithmatic {
+	public static void main(String[] args) {
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println("Addition is : "+c);
+		c=a-b;
+		System.out.println("Subtraction is : "+c);
+		c=a*b;
+		System.out.println("Multiplication is : "+c);
+		c=a%b;
+		System.out.println("Module is : "+c);
+		c=a/b;
+		System.out.println("Division is : "+c);
+	}
+}

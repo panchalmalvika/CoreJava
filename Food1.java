412389a581afffe784b349d7fdf1ab2048b8da8c
Panchal Malvika <panchalmalvika944@gmail.com>
2025-07-18 14:49:00 +0530

    18-07-2025

---
+++ Food1.java
@@ -0,0 +1,256 @@
+package Test.first;
+
+import java.util.Scanner;
+
+public class Food1 {
+
+		public static void main(String[] args) {
+			
+			int choise,quantity,total = 0,total1 = 0;
+			int yes=1,no=0;
+			System.out.println("\t\t*************************************** WELCOME TO FOOD BILLING SYSTEM ***************************************");
+			System.out.println("\n\t\t1.Dosa\t\tPrize = 180rs/pcs");
+			System.out.println("\t\t2.Samosa\tPrize = 20rs/pcs");
+			System.out.println("\t\t3.Idali Vada\tPrize = 50rs/pcs");
+			System.out.println("\t\t4.Alu Parotha\tPrize = 100rs/pcs");
+			System.out.println("\t\t5.Pav Bhaji\tPrize = 120rs/pcs");
+			System.out.println("\t\t6.Pulav\t\tPrize = 80rs/pcs");
+			System.out.println("\t\t7.Pani Puri\tPrize = 20rs/6pcs");
+			System.out.println("\t\t8.Alu Puri\tPrize = 100rs/pcs");
+			System.out.println("\n\t\t**************************************************************************************************************");
+			System.out.println("\n\t\t\t\t\t\t\tAt a time One Choise is accept");
+			Scanner s=new Scanner(System.in);
+			System.out.print("\n\t\t\t\t\t\t\tChoose one Option....:");
+			choise=s.nextInt();
+			System.out.println("\n\t\t**************************************************************************************************************");
+			if(choise==1)
+			{
+					System.out.println("\n\t\t\t\t\t\t\tYour choise is : Dosa");
+					System.out.print("\n\t\t\t\t\t\t\tEnter the Quantity : ");
+					quantity=s.nextInt();
+					total=quantity*180;
+					System.out.println("\n\t\t**************************************************************************************************************");
+					System.out.println("\n\t\t\t\t\t\t\t\t\tTotal Amount is : "+total);
+					System.out.println("\n\t\t**************************************************************************************************************");
+			}
+			else if(choise==2)
+			{
+					System.out.println("\n\t\t\t\t\t\t\tYour choise is : Samosa");
+					System.out.print("\n\t\t\t\t\t\t\tEnter the Quantity : ");
+					quantity=s.nextInt();
+					total=quantity*20;
+					System.out.println("\n\t\t**************************************************************************************************************");
+					System.out.println("\n\t\t\t\t\t\t\tTotal Amount is : "+total);
+					System.out.println("\n\t\t**************************************************************************************************************");
+			}
+			else if(choise==3)
+			{
+					System.out.println("\n\t\t\t\t\t\t\tYour choise is : Idali Vada");
+					System.out.print("\n\t\t\t\t\t\t\tEnter the Quantity : ");
+					quantity=s.nextInt();
+					total=quantity*50;
+					System.out.println("\n\t\t**************************************************************************************************************");
+					System.out.println("\n\t\t\t\t\t\t\tTotal Amount is : "+total);
+					System.out.println("\n\t\t**************************************************************************************************************");
+			}
+			else if(choise==4)
+			{
+					System.out.println("\n\t\t\t\t\t\t\tYour choise is : Alu Parotha");
+					System.out.print("\n\t\t\t\t\t\t\tEnter the Quantity : ");
+					quantity=s.nextInt();
+					total=quantity*100;
+					System.out.println("\n\t\t**************************************************************************************************************");
+					System.out.println("\n\t\t\t\t\t\t\tTotal Amount is : "+total);
+					System.out.println("\n\t\t**************************************************************************************************************");
+			}
+			else if(choise==5)
+			{
+					System.out.println("\n\t\t\t\t\t\t\tYour choise is : Pav Bhaji");
+					System.out.print("\n\t\t\t\t\t\t\tEnter the Quantity : ");
+					quantity=s.nextInt();
+					total=quantity*120;
+					System.out.println("\n\t\t**************************************************************************************************************");
+					System.out.println("\n\t\t\t\t\t\t\tTotal Amount is : "+total);
+					System.out.println("\n\t\t**************************************************************************************************************");
+			}
+			else if(choise==6)
+			{
+					System.out.println("\n\t\t\t\t\t\t\tYour choise is : Pulav");
+					System.out.print("\n\t\t\t\t\t\t\tEnter the Quantity : ");
+					quantity=s.nextInt();
+					total=quantity*80;
+					System.out.println("\n\t\t**************************************************************************************************************");
+					System.out.println("\n\t\t\t\t\t\t\tTotal Amount is : "+total);
+					System.out.println("\n\t\t**************************************************************************************************************");
+			}
+			else if(choise==7)
+			{
+					System.out.println("\n\t\t\t\t\t\t\tYour choise is : Pani Puri");
+					System.out.print("\n\t\t\t\t\t\t\tEnter the Quantity : ");
+					quantity=s.nextInt();
+					total=quantity*20;
+					System.out.println("\n\t\t**************************************************************************************************************");
+					System.out.println("\n\t\t\t\t\t\t\tTotal Amount is : "+total);
+					System.out.println("\n\t\t**************************************************************************************************************");
+			}
+			else if(choise==8)
+			{
+					System.out.println("\n\t\t\t\t\t\t\tYour choise is : Alu Puri");
+					System.out.print("\n\t\t\t\t\t\t\tEnter the Quantity : ");
+					quantity=s.nextInt();
+					total=quantity*100;
+					System.out.println("\n\t\t**************************************************************************************************************");
+					System.out.println("\n\t\t\t\t\t\t\tTotal Amount is : "+total);
+					System.out.println("\n\t\t**************************************************************************************************************");
+			}
+			else
+			{
+					System.out.println("Invalid choise...");
+			}
+			System.out.println("\n\t\t**************************************************************************************************************");
+			System.out.print("\n\t\t\t\t\t\t\tDo you want More Order ?");
+			System.out.print("\n\t\t\t\t\t\tIf Yes the select 1 or no then select 0:");
+			yes=s.nextInt();
+			if(yes==1)
+			{
+				System.out.println("\t\t*************************************** WELCOME TO FOOD BILLING SYSTEM ***************************************");
+				System.out.println("\n\t\t1.Dosa\t\tPrize = 180rs/pcs");
+				System.out.println("\t\t2.Samosa\tPrize = 20rs/pcs");
+				System.out.println("\t\t3.Idali Vada\tPrize = 50rs/pcs");
+				System.out.println("\t\t4.Alu Parotha\tPrize = 100rs/pcs");
+				System.out.println("\t\t5.Pav Bhaji\tPrize = 120rs/pcs");
+				System.out.println("\t\t6.Pulav\t\tPrize = 80rs/pcs");
+				System.out.println("\t\t7.Pani Puri\tPrize = 20rs/6pcs");
+				System.out.println("\t\t8.Alu Puri\tPrize = 100rs/pcs");
+				System.out.println("\n\t\t**************************************************************************************************************");
+				System.out.println("\n\t\t\t\t\t\t\tAt a time One Choise is accept");
+				System.out.print("\n\t\t\t\t\t\t\tChoose one Option....:");
+				choise=s.nextInt();
+				System.out.println("\n\t\t**************************************************************************************************************");
+				if(choise==1)
+				{
+						System.out.println("\n\t\t\t\t\t\t\tYour choise is : Dosa");
+						System.out.print("\n\t\t\t\t\t\t\tEnter the Quantity : ");
+						quantity=s.nextInt();
+						total1=total+quantity*180;
+						System.out.println("\n\t\t**************************************************************************************************************");
+						System.out.println("\n\t\t\t\t\t\t\t\t\tTotal Amount is : "+total1);
+						System.out.println("\n\t\t**************************************************************************************************************");
+						System.out.println("\n\t\t\t\t\t\t\tTHANK YOU FOR ORDERING FOOD!");
+						System.out.println("\n\t\t\t\t\t\t\tPlease Pay Your Bill..:"+total1+" Rs. ");
+						System.out.println("\n\t\t**************************************************************************************************************");
+				}
+				else if(choise==2)
+				{
+						System.out.println("\n\t\t\t\t\t\t\tYour choise is : Samosa");
+						System.out.print("\n\t\t\t\t\t\t\tEnter the Quantity : ");
+						quantity=s.nextInt(); 
+						total1=total+quantity*20;
+						System.out.println("\n\t\t**************************************************************************************************************");
+						System.out.println("\n\t\t\t\t\t\t\tTotal Amount is : "+total1);
+						System.out.println("\n\t\t**************************************************************************************************************");
+						System.out.println("\n\t\t\t\t\t\t\tTHANK YOU FOR ORDERING FOOD!");
+						System.out.println("\n\t\t\t\t\t\t\tPlease Pay Your Bill..:"+total1+" Rs. ");
+						System.out.println("\n\t\t**************************************************************************************************************");
+				}
+				else if(choise==3)
+				{
+						System.out.println("\n\t\t\t\t\t\t\tYour choise is : Idali Vada");
+						System.out.print("\n\t\t\t\t\t\t\tEnter the Quantity : ");
+						quantity=s.nextInt();
+						total1=total+quantity*50;
+						System.out.println("\n\t\t**************************************************************************************************************");
+						System.out.println("\n\t\t\t\t\t\t\tTotal Amount is : "+total1);
+						System.out.println("\n\t\t**************************************************************************************************************");
+						System.out.println("\n\t\t\t\t\t\t\tTHANK YOU FOR ORDERING FOOD!");
+						System.out.println("\n\t\t\t\t\t\t\tPlease Pay Your Bill..:"+total1+" Rs. ");
+						System.out.println("\n\t\t**************************************************************************************************************");
+				}
+				else if(choise==4)
+				{
+						System.out.println("\n\t\t\t\t\t\t\tYour choise is : Alu Parotha");
+						System.out.print("\n\t\t\t\t\t\t\tEnter the Quantity : ");
+						quantity=s.nextInt();
+						total1=total+quantity*100;
+						System.out.println("\n\t\t**************************************************************************************************************");
+						System.out.println("\n\t\t\t\t\t\t\tTotal Amount is : "+total1);
+						System.out.println("\n\t\t**************************************************************************************************************");
+						System.out.println("\n\t\t\t\t\t\t\tTHANK YOU FOR ORDERING FOOD!");
+						System.out.println("\n\t\t\t\t\t\t\tPlease Pay Your Bill..:"+total1+" Rs.");
+						System.out.println("\n\t\t**************************************************************************************************************");
+				}
+				else if(choise==5)
+				{
+						System.out.println("\n\t\t\t\t\t\t\tYour choise is : Pav Bhaji");
+						System.out.print("\n\t\t\t\t\t\t\tEnter the Quantity : ");
+						quantity=s.nextInt();
+						total1=total+quantity*120;
+						System.out.println("\n\t\t**************************************************************************************************************");
+						System.out.println("\n\t\t\t\t\t\t\tTotal Amount is : "+total1);
+						System.out.println("\n\t\t**************************************************************************************************************");
+						System.out.println("\n\t\t\t\t\t\t\tTHANK YOU FOR ORDERING FOOD!");
+						System.out.println("\n\t\t\t\t\t\t\tPlease Pay Your Bill..:"+total1+" Rs. ");
+						System.out.println("\n\t\t**************************************************************************************************************");
+				}
+				else if(choise==6)
+				{
+						System.out.println("\n\t\t\t\t\t\t\tYour choise is : Pulav");
+						System.out.print("\n\t\t\t\t\t\t\tEnter the Quantity : ");
+						quantity=s.nextInt();
+						total1=total+quantity*80;
+						System.out.println("\n\t\t**************************************************************************************************************");
+						System.out.println("\n\t\t\t\t\t\t\tTotal Amount is : "+total1);
+						System.out.println("\n\t\t**************************************************************************************************************");
+						System.out.println("\n\t\t\t\t\t\t\tTHANK YOU FOR ORDERING FOOD!");
+						System.out.println("\n\t\t\t\t\t\t\tPlease Pay Your Bill..:"+total1+" Rs. ");
+						System.out.println("\n\t\t**************************************************************************************************************");
+				}
+				else if(choise==7)
+				{
+						System.out.println("\n\t\t\t\t\t\t\tYour choise is : Pani Puri");
+						System.out.print("\n\t\t\t\t\t\t\tEnter the Quantity : ");
+						quantity=s.nextInt();
+						total1=total+quantity*20;
+						System.out.println("\n\t\t**************************************************************************************************************");
+						System.out.println("\n\t\t\t\t\t\t\tTotal Amount is : "+total1);
+						System.out.println("\n\t\t**************************************************************************************************************");
+						System.out.println("\n\t\t\t\t\t\t\tTHANK YOU FOR ORDERING FOOD!");
+						System.out.println("\n\t\t\t\t\t\t\tPlease Pay Your Bill..:"+total1+" Rs. ");
+						System.out.println("\n\t\t**************************************************************************************************************");
+				}
+				else if(choise==8)
+				{
+						System.out.println("\n\t\t\t\t\t\t\tYour choise is : Alu Puri");
+						System.out.print("\n\t\t\t\t\t\t\tEnter the Quantity : ");
+						quantity=s.nextInt();
+						total1=total+quantity*100;
+						System.out.println("\n\t\t**************************************************************************************************************");
+						System.out.println("\n\t\t\t\t\t\t\tTotal Amount is : "+total1);
+						System.out.println("\n\t\t**************************************************************************************************************");
+						System.out.println("\n\t\t\t\t\t\t\tTHANK YOU FOR ORDERING FOOD!");
+						System.out.println("\n\t\t\t\t\t\t\tPlease Pay Your Bill..:"+total1+" Rs. ");
+						System.out.println("\n\t\t**************************************************************************************************************");
+				}
+				else
+				{
+						System.out.println("Invalid choise...");
+				}
+			}
+			else if(no==0)
+			{
+					System.out.println("\n\t\t**************************************************************************************************************");
+					System.out.println("\n\t\t\t\t\t\t\tTHANK YOU FOR ORDERING FOOD!");
+					System.out.println("\n\t\t\t\t\t\t\tPlease Pay Your Bill..:"+(total1+total)+" Rs. ");
+					System.out.println("\n\t\t**************************************************************************************************************");
+			}
+			else
+			{
+				System.out.println("\n\t\t**************************************************************************************************************");
+				System.out.println("\n\t\t\t\t\tTHANK YOU FOR ORDERING FOOD!");
+				System.out.println("\n\t\t**************************************************************************************************************");
+			}
+	}
+	}
+
+
+

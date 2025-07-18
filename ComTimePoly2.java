9f0592cdab9e9ca5eb8d13f810d5be56f95bfa01
Panchal Malvika <panchalmalvika944@gmail.com>
2025-07-18 14:51:14 +0530

    18-07-2025

---
+++ ComTimePoly2.java
@@ -0,0 +1,21 @@
+package Test.first;
+
+public class ComTimePoly2 {
+	static void test()
+	{
+		System.out.println("Test with no argument");
+	}
+	void test(int a)
+	{
+		System.out.println("Test with one argument");
+	}
+	void test(int a,int b)
+	{
+		System.out.println("Test with two argument");
+	}
+	public static void main(String[] args) {
+		test();
+	}
+}
+
+

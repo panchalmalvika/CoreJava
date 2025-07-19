509949ab4a15acbe3dc263022efcb3310b09cd65
Panchal Malvika <panchalmalvika944@gmail.com>
2025-07-19 10:52:45 +0530

    19-07-2025

---
+++ ifelsesta.java
@@ -0,0 +1,15 @@
+package Test.first;
+
+public class ifelsesta {
+	public static void main(String[] args) {
+		int a=10,b=20;
+		if(a<b)
+		{
+			System.out.println("b is max");
+		}
+		else
+		{
+			System.out.println("a is max");
+		}
+	}
+}

6d5319a9a8e0c811c9e32cb00d81b34d11f0dabd
Panchal Malvika <panchalmalvika944@gmail.com>
2025-07-19 10:47:53 +0530

    19-07-2025

---
+++ elseifsta.java
@@ -0,0 +1,27 @@
+package Test.first;
+
+public class elseifsta {
+	public static void main(String[] args) {
+		int total=88;
+		if(total>=90)
+		{
+			System.out.println("First rank:");
+		}
+		else if(total>=80)
+		{
+			System.out.println("Second rank:");
+		}
+		else if(total>=70)
+		{
+			System.out.println("Third rank:");
+		}
+		else if(total>=55)
+		{
+			System.out.println("pass rank:");
+		}
+		else
+		{
+			System.out.println("fail");
+		}
+	}
+}

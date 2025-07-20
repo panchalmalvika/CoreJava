1a9c330b7de2b5bda758deb8b820ee901d4fab3f
Panchal Malvika <panchalmalvika944@gmail.com>
2025-07-20 08:27:58 +0530

    20-07-2025

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+package Test.first;
+
+public class Pattern {
+	public static void main(String[] args) {
+		int i,j,k;
+		for(i=0;i<=10;i++)
+		{
+			for(j=0;j<10-i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(k=0;k<=i;k++)
+			{
+				System.out.print(" *");
+			}
+			System.out.print("\n");
+		}
+	}
+}

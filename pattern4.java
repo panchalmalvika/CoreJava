1050f0e54851e250cc144d9a59eb517da919a329
Panchal Malvika <panchalmalvika944@gmail.com>
2025-07-21 10:03:34 +0530

    21-07-2025

---
+++ pattern4.java
@@ -0,0 +1,15 @@
+package Test.first;
+
+public class pattern4 {
+	public static void main(String[] args) {
+		int i,j;
+		for(i=0;i<10;i++)
+		{
+			for(j=0;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+	}
+}

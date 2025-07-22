07e54c58027dd7def518051e65d20c573d0a24c0
Panchal Malvika <panchalmalvika944@gmail.com>
2025-07-22 22:11:01 +0530

    22-07-2025

---
+++ TryCatchDemo.java
@@ -0,0 +1,16 @@
+package Test.first;
+
+public class TryCatchDemo {
+
+	public static void main(String[] args) {
+	    try {
+	      int[] myNumbers = {1, 2, 3};
+	      System.out.println(myNumbers[10]);
+	    } catch (Exception e) {
+	      System.out.println("Something went wrong.");
+	    } finally {
+	      System.out.println("The 'try catch' is finished.");
+	    }
+	  }
+	}
+

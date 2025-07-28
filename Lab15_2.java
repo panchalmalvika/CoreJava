db8c072d6319a445f0bd9388d4bb96d62ba7e916
Panchal Malvika <panchalmalvika944@gmail.com>
2025-07-28 16:42:12 +0530

    28-07-2025

---
+++ Lab15_2.java
@@ -0,0 +1,20 @@
+package assignment;
+
+import java.util.HashSet;
+
+public class Lab15_2 {
+
+	public static void main(String[] args) {
+		HashSet hs=new HashSet();
+		
+		hs.add("Panchal Malvika");
+		hs.add('M');
+		hs.add(90);
+		hs.add(90.98);
+		hs.add(true);
+		hs.add(null);
+		hs.add(90);
+		hs.add(9);
+		System.out.println(hs);
+	}
+}

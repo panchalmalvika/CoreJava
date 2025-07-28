b6d65db99fc656eeb3ee2c9cab9d75af501ca99e
Panchal Malvika <panchalmalvika944@gmail.com>
2025-07-28 16:42:29 +0530

    28-07-2025

---
+++ Lab15_3.java
@@ -0,0 +1,21 @@
+package assignment;
+
+import java.util.HashMap;
+
+public class Lab15_3 {
+
+	public static void main(String[] args) {
+		
+		HashMap hm=new HashMap();
+		
+		hm.put(101, "Panchal Malvika");
+		hm.put(102, "Darji Jagrat");
+		hm.put(103, 'M');
+		hm.put(104, null);
+		hm.put(105, true);
+		hm.put(106, 90);
+		hm.put(107, 90.98);
+		
+		System.out.println(hm);
+	}
+}

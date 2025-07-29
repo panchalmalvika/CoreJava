6cfe583d7a36eee1d42261f256ad05dd36b78ee1
Panchal Malvika <panchalmalvika944@gmail.com>
2025-07-29 10:33:41 +0530

    29-07-2025

---
+++ MyServlet2.java
@@ -0,0 +1,37 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/MyServlet2")
+public class MyServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+   		ServletConfig config;
+   	    ServletContext context;
+   	    String email,driver;
+   		public void init(ServletConfig config) throws ServletException{
+   			this.config=config;
+   			this.context=config.getServletContext();
+   		}
+   		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   			response.setContentType("text/html");
+   			PrintWriter out=response.getWriter();
+   			email=config.getInitParameter("email");
+   			System.out.println("Email : "+email);
+   			out.println("Email : "+email);
+   			
+   			driver=context.getInitParameter("driver");
+   			System.out.println("Driver : "+driver);
+   			out.println("\nDriver : "+driver);
+   		}   		
+}
+

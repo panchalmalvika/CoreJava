dd98c80322d8cfe75753b86e1840c1ac4193d877
Panchal Malvika <panchalmalvika944@gmail.com>
2025-08-01 11:18:59 +0530

    01-08-2025

--- MyServlet.java
+++ MyServlet.java
@@ -1,41 +1,18 @@
 package com.servlet;
 
-import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.InterruptedIOException;
-import java.io.PrintWriter;
 
 @WebServlet("/MyServlet")
 public class MyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	ServletContext context;
-
-	public void init(ServletConfig config) throws ServletException {
-		this.context=config.getServletContext();
-	}
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		PrintWriter out=response.getWriter();
-		
-		int counter=1;
-		
-		if(context.getAttribute("counter")!=null)
-		{
-			counter=Integer.parseInt(context.getAttribute("counter").toString());
-		}
-		context.setAttribute("counter",counter);
-		out.println("You Are Visiting This Web Page "+counter+" Times");
-		
-	}
+     
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	System.out.println("Login Succesfully");	
 	}
 
 }

eb45add5452e6b62b907ba49ccf111a639a0ab3c
Panchal Malvika <panchalmalvika944@gmail.com>
2025-08-01 11:14:03 +0530

    01-08-2025

---
+++ UserController.java
@@ -0,0 +1,78 @@
+package com.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+import com.Dao.UserDao;
+import com.user.User;
+
+@WebServlet("/UserController")
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action=request.getParameter("action");
+		if(action.equalsIgnoreCase("sign up"))
+		{
+			boolean flag=UserDao.checkEmail(request.getParameter("email"));
+			if(flag==false)
+			{
+				if(request.getParameter("password").equals(request.getParameter("cpassword")))
+				{
+					User u=new User();
+					u.setFname(request.getParameter("fname"));
+					u.setLname(request.getParameter("lname"));
+					u.setEmail(request.getParameter("email"));
+					u.setMobile(request.getParameter("mobile"));
+					u.setAddress(request.getParameter("address"));
+					u.setPassword(request.getParameter("password"));
+					UserDao.singupUser(u);
+					request.setAttribute("msg","User Sign Up Successfull");
+					request.getRequestDispatcher("login.jsp").forward(request, response);
+				}
+				else
+				{
+					request.setAttribute("msg", "Password & Confirm Password Does Not Matched");
+					request.getRequestDispatcher("login.jsp").forward(request, response);
+				}
+			}
+			else
+			{
+				request.setAttribute("msg","Email Already Registered");
+				request.getRequestDispatcher("signup.jsp").forward(request, response);
+			}
+		}
+		else if(action.equalsIgnoreCase("login"))
+		{
+			User u=UserDao.loginUser(request.getParameter("email"));
+			if(u!=null)
+			{
+				if(u.getPassword().equals(request.getParameter("password")))
+				{
+					HttpSession session=request.getSession();
+					session.setAttribute("u", u);
+					request.getRequestDispatcher("home.jsp").forward(request, response);
+				}
+				else
+				{
+					request.setAttribute("msg", "Incorrect Password");
+					request.getRequestDispatcher("login.jsp").forward(request, response);
+				}
+			}
+			else
+			{
+				request.setAttribute("msg", "Email Not Registered");
+				request.getRequestDispatcher("login.jsp").forward(request, response);
+			}
+		}
+	}
+
+}
+

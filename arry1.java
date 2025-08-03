ce842a24af718fd38721bb0287ae24571120e129
Panchal Malvika <panchalmalvika944@gmail.com>
2025-08-03 15:38:06 +0530

    03-08-2025

---
+++ arry1.java
@@ -0,0 +1,16 @@
+class arry1
+{
+public static void main(String arg[])
+{
+int ar1[][]={{10,11,12},{21,22,23},{31,32,33}};
+
+for(int i=0;i<ar1.length;i++)
+{
+for(int j=0;j<ar1[i].length;j++){
+System.out.println("ar1["+i+"]["+j+"]="+ar1{[i][j]+"");
+}
+System.out.println();
+}
+
+}
+}

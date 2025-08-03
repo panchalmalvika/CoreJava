11460ab90174b453244130c6593b52e900466952
Panchal Malvika <panchalmalvika944@gmail.com>
2025-08-03 15:41:34 +0530

    03-08-2025

---
+++ xyz.java
@@ -0,0 +1,14 @@
+class sale{
+int a=11;
+void j1(){
+System.out.println("car on sale");
+System.out.println(a);
+}
+}
+class xyz{
+public static void main(String arg[])
+{
+sale s1 =new sale();
+s1.j1();
+}
+}

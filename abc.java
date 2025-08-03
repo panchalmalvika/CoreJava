99775d9370706b02cb170c1d8f34904191c9fbd3
Panchal Malvika <panchalmalvika944@gmail.com>
2025-08-03 15:37:30 +0530

    03-08-2025

---
+++ abc.java
@@ -0,0 +1,15 @@
+class cons
+void showDetails()
+{
+System.out.println("hello");
+	
+}
+class abc
+{
+	public static void main(String arg[])
+	{
+		cons s1 = new cons(); 	//Call default Constructor
+		s1.showDetails();
+	}
+}
+

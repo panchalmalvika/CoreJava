18d1aae4a768b0534a50ce5c6cdd475f9ae981dd
Panchal Malvika <panchalmalvika944@gmail.com>
2025-08-03 15:40:13 +0530

    03-08-2025

---
+++ demo1.java
@@ -0,0 +1,22 @@
+class demo1{
+public static void main(String arg[])
+{
+int a=90;
+if(a>=80 && a<=70);
+{
+	System.out.println("A grade:");
+}
+elseif(a>=70 && a<=60);
+{
+	System.out.println("B grade:");
+}
+elseif(a>=60 && a<=50);
+{
+	System.out.println("C grade:");
+}
+else{
+System.out.println("d grade:");
+
+}
+}
+}

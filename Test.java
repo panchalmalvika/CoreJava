7c8ecffe1a457d5480858be4c0bf132b3d06e776
Panchal Malvika <panchalmalvika944@gmail.com>
2025-08-04 09:39:37 +0530

    04-08-2025

---
+++ Test.java
@@ -0,0 +1,17 @@
+package test.first;
+
+public class Test {
+	public static void main(String[] args) {
+		int a=20;
+		if(a<=10)
+		{
+				System.out.println("a is min");	
+		}
+		else
+		{
+				System.out.println("a is max");
+		}
+		
+	}
+}
+
